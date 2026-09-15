eb1f8f41b00c6ee7fba3692262adef9e3763ee11
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-15 21:20:19 +0545

    feat: create sqlalchemy engine to connect to local mysql server

--- database/connection.py
+++ database/connection.py
@@ -12,3 +12,5 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
