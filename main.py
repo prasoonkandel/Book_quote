4f1652af5321d49d67e1f1a9a0b671866813eb10
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-15 13:03:32 +0545

    chore: init fastapi setup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("jai ganesh!")
+from tkinter.constants import PAGES
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return {"message": "welcome to home page <3"}
+
+
+@app.get("/text/{text}")
+async def getTxt(text: str):
+    return {"text": text}
