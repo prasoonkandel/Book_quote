d42e971658546ae75ba851ffad1bbd8bed05eb2e
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-15 19:37:29 +0545

    feat: made program to connect to the mysql database

---
+++ database/connection.py
@@ -0,0 +1,15 @@
+import os
+from tarfile import OutsideDestinationError
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
