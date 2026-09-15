34ac8defe34c930ffea8c0aa605f673a8565a93f
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-15 19:02:48 +0545

    chore: made program to embed all quotes

--- utils/embed.py
+++ utils/embed.py
@@ -10,26 +10,35 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_API_URL = os.getenv("OPENROUTER_API_URL")
 
+
 if not OPENROUTER_API_KEY:
     raise ValueError("Missing OPENROUTER_API_KEY in your .env file!")
 
-url = OPENROUTER_API_URL
 headers = {
     "Authorization": f"Bearer {OPENROUTER_API_KEY}",
     "Content-Type": "application/json",
 }
-data = {
-    "model": "qwen/qwen3-embedding-4b",
-    "input": "The quick brown fox jumps over the lazy dog",
-}
-response = requests.post(url, headers=headers, json=data)
 
-response.raise_for_status()
 
-response_json = response.json()
+def get_embedding(text):
+    data = {
+        "model": "qwen/qwen3-embedding-4b",
+        "input": text,
+    }
 
-embedding = response_json["data"][0]["embedding"]
-print(embedding)
+    response = requests.post(
+        OPENROUTER_API_URL,
+        headers=headers,
+        json=data,
+    )
 
-# Read your CSV file
+    response.raise_for_status()
+
+    return response.json()["data"][0]["embedding"]
+
+
 df = pd.read_csv("utils/quote.csv")
+
+df["embedding"] = df["Quote"].apply(get_embedding)
+
+df.to_csv("utils/quote.csv", index=False)
