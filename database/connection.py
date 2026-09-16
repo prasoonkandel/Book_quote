c4f057d2ba072f589ad25631fe0ac42087a5773c
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-16 21:46:25 +0545

    fix: improve database connection logic

--- database/connection.py
+++ database/connection.py
@@ -1,12 +1,13 @@
 import os
+from urllib.parse import quote_plus
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_USER = quote_plus(os.getenv("DB_USER") or "")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD") or "")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
