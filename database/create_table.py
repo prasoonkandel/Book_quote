213f76e650dbcf9fbfb2c9d0d4b7d4310b10ac0a
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-16 22:26:04 +0545

    fix: differenciate create_table and seed logic in different modules

---
+++ database/create_table.py
@@ -0,0 +1,16 @@
+from sqlalchemy import text
+
+from database.connection import engine
+
+with engine.begin() as conn:
+    conn.execute(
+        text("""
+            CREATE TABLE IF NOT EXISTS quotes (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                quote TEXT NOT NULL,
+                author TEXT
+            );
+        """)
+    )
+
+print("Table created successfully!")
--- database/seed.py
+++ database/seed.py
@@ -9,19 +9,4 @@ df = df[["quote", "author"]]
 
 df = df.dropna(subset=["quote", "author"])
 
-with engine.begin() as conn:
-    conn.execute(
-        text("""
-            CREATE TABLE IF NOT EXISTS quotes (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                quote TEXT NOT NULL,
-                author TEXT
-            );
-        """)
-    )
-
-print("Table created successfully!")
-
 df.to_sql(name="quotes", con=engine, if_exists="append", index=False, chunksize=5000)
-
-print("Data seeded successfully!")
