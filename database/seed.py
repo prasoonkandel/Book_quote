cae822b1c97b42baa312f4f11f0a9e55ef87e268
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-16 22:14:58 +0545

    feat: complete module to seed the database

--- database/seed.py
+++ database/seed.py
@@ -3,4 +3,25 @@ from sqlalchemy import text
 
 from database.connection import engine
 
-df = pd.read_csv("data/quote.csv")
+df = pd.read_csv("data/quotes.csv")
+
+df = df[["quote", "author"]]
+
+df = df.dropna(subset=["quote", "author"])
+
+with engine.begin() as conn:
+    conn.execute(
+        text("""
+            CREATE TABLE IF NOT EXISTS quotes (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                quote TEXT NOT NULL,
+                author TEXT
+            );
+        """)
+    )
+
+print("Table created successfully!")
+
+df.to_sql(name="quotes", con=engine, if_exists="append", index=False, chunksize=5000)
+
+print("Data seeded successfully!")
