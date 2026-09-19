7d94d1e22d565740bf7a07b3ebebc4261a39f64b
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 22:16:41 +0545

    feat: add function to find cosine similarity

--- services/vector.py
+++ services/vector.py
@@ -1,5 +1,10 @@
 import numpy as np
+import pandas as pd
 
 
 def normalize_vector(vector):
     return vector / np.linalg.norm(vector)
+
+
+def cosine_similarity(A, B):
+    return np.dot(A, B) / (np.linalg.norm(A) * np.linalg.norm(B))
