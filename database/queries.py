7b5364a80dffb46a308646ff326d61e05ed2b578
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 14:37:57 +0545

    fix: making database functions more readable

--- database/queries.py
+++ database/queries.py
@@ -5,11 +5,17 @@ from database.connection import engine
 
 def get_all_quotes():
     with engine.begin() as conn:
+        result = conn.execute(text("SELECT quote FROM quotes"))
+        return result.fetchall()
+
+
+def get_all_quotes_data():
+    with engine.begin() as conn:
         result = conn.execute(text("SELECT * FROM quotes"))
         return result.fetchall()
 
 
-def get_quote(quote_id):
+def get_quote_data(quote_id):
     with engine.begin() as conn:
         result = conn.execute(text(f"SELECT * FROM quotes WHERE id = {quote_id}"))
         return result.fetchone()
