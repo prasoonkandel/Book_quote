53b9332ea0ac9f297d5b488bdee3177fe150d415
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 19:59:29 +0545

    fix: preventing queries from sql injection

--- database/queries.py
+++ database/queries.py
@@ -4,30 +4,32 @@ from database.connection import engine
 
 
 def get_all_quotes():
-    with engine.begin() as conn:
+    with engine.connect() as conn:
         result = conn.execute(text("SELECT quote FROM quotes"))
         return result.fetchall()
 
 
 def get_all_quotes_data():
-    with engine.begin() as conn:
+    with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM quotes"))
         return result.fetchall()
 
 
 def get_quote_data(quote_id):
-    with engine.begin() as conn:
-        result = conn.execute(text(f"SELECT * FROM quotes WHERE id = {quote_id}"))
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM quotes WHERE id = :quote_id"), {"quote_id": quote_id}
+        )
         return result.fetchone()
 
 
 def get_max_id():
-    with engine.begin() as conn:
+    with engine.connect() as conn:
         result = conn.execute(text("SELECT MAX(id) FROM quotes"))
         return result.fetchone()
 
 
 def get_min_id():
-    with engine.begin() as conn:
+    with engine.connect() as conn:
         result = conn.execute(text("SELECT MIN(id) FROM quotes"))
         return result.fetchone()
