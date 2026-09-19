3a5261d07ffcaf6d49c383025ae3c261e0e653d9
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 23:12:31 +0545

    fix: converting lists to vectors upon requirements

--- services/quote_service.py
+++ services/quote_service.py
@@ -21,7 +21,9 @@ def get_top_quotes(query: str, k: int = 20):
     try:
         query_embedding = get_embedding(query)
         top_k = search_top_k(query_embedding, k)
+        query_embedding = np.array(query_embedding)
         df = pd.DataFrame(top_k, columns=["id", "vector"])
+        df["vector"] = df["vector"].apply(np.array)
         df = merge_quote_data(df, get_full_quote_data(df["id"]))
         df = similarity_rank(query_embedding, df)
         df = df[["quote", "author", "similarity"]]
