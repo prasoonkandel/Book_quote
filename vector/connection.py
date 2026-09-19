cc1630f01f53b56348f4a04bababbc85a6c16498
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 14:35:53 +0545

    feat: made pinecone db connection module in vector package

---
+++ vector/connection.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+from pinecone import Pinecone
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+index = pc.Index(host=os.getenv("PINECONE_INDEX_HOST"))
