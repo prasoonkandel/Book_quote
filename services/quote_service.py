00b3b44e4807cf092f9ce93e93fa7bf3754fbcec
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 23:25:44 +0545

    fix: critical issues in function arguments

--- services/quote_service.py
+++ services/quote_service.py
@@ -24,7 +24,8 @@ def get_top_quotes(query: str, k: int = 20):
         query_embedding = np.array(query_embedding)
         df = pd.DataFrame(top_k, columns=["id", "vector"])
         df["vector"] = df["vector"].apply(np.array)
-        df = merge_quote_data(df, get_full_quote_data(df["id"].tolist()))
+        df["id"] = df["id"].astype(int)
+        df = merge_quote_data(get_full_quote_data(df["id"].tolist()), df)
         df = similarity_rank(query_embedding, df)
         df = df[["quote", "author", "similarity"]]
         return df.to_dict(orient="records")
