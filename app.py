3de30011bfb48e6982ee9a8f4976a9b3385d8da4
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-19 23:03:07 +0545

    feat: made api endpoint to get top quotes

--- app.py
+++ app.py
@@ -13,3 +13,12 @@ class Quote(BaseModel):
 @app.get("/")
 def read_root():
     return {"message": "Welcome to Book Quote"}
+
+
+@app.post("/get-quotes")
+def get_quotes(quote: Quote):
+    try:
+        top_quotes = get_top_quotes(quote.query, quote.count)
+        return {"quotes": top_quotes}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
