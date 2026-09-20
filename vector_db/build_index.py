5c91b3ac75e44e0f1283af201a75f5ed07b3b55a
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-09-20 12:35:00 +0545

    fix: change batch size of embeding and upsert to pinecone to 500

--- vector_db/build_index.py
+++ vector_db/build_index.py
@@ -7,7 +7,7 @@ from vector_db.connection import index, pc
 MIN = db.get_min_id()[0]
 MAX = db.get_max_id()[0]
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 500
 
 for i in range(MIN, MAX + 1, BATCH_SIZE):
     batch_end = min(i + BATCH_SIZE - 1, MAX)
